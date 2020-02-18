eb2132d942d1db849fbe3022b275dbd747912503
rapidsotakusub <suxiaotuo@126.com>
2020-02-18 11:13:26 +0900

    StagePlayerRotation

--- BasicGameTemplate/Assets/7_個人/蘇/StageRotation.cs
+++ BasicGameTemplate/Assets/7_個人/蘇/StageRotation.cs
@@ -4,144 +4,129 @@ using UnityEngine;
 
 public class StageRotation : MonoBehaviour
 {
-    float CurrentAngle;
-    float OldAngle;
+    //Stageについて
+    float rotateSpeed = 2f;
+    Quaternion currentTargetAngels;
+    Quaternion oldTargetAngels;
     bool LeftRotateFlag = false;
     bool RightRotateFlag = false;
+    public  Rigidbody Player;
+    public Transform PlayerTransform;
+    public static bool MoveFlag = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        CurrentAngle = 0f;
-        OldAngle = 0f;
+        // Quaternion.Slerp()第二个参数需要的是四元数,所以这里需要将目标的角度转成四元数去计算
+        currentTargetAngels = Quaternion.Euler(0, 0, 0f);
+        oldTargetAngels = Quaternion.Euler(0, 0, 0f);
+
+        Player.useGravity = true;  //Default状況でPlayerの重力があります
+        MoveFlag = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //transform.Rotate(Vector3.forward * m_RotateSpeed * Time.deltaTime, m_RotateSpace);
-
-
-        //do something
-        //GetAxis(全部で７Axis)
-        float LeftStickHorizontal = Input.GetAxis("LeftStickHorizontal");
-        float LeftStickVertical = Input.GetAxis("LeftStickVertical");
-        float LeftCrossHorizontal = Input.GetAxis("LeftCrossHorizontal");
-        float LeftCrossVertical = Input.GetAxis("LeftCrossVertical");
-        float RightStickHorizontal = Input.GetAxis("RightStickHorizontal");
-        float RightStickVertical = Input.GetAxis("RightStickVertical");
-        float LRTrigger = Input.GetAxis("LRTrigger");
-
-        //左スティック
-        if (Mathf.Abs(LeftStickHorizontal) > 0.05f || Mathf.Abs(LeftStickVertical) > 0.05f)
-        {
-            print("LeftStickHorizontal:" + LeftStickHorizontal);
-            print("LeftStickVertical:" + LeftStickVertical);
-        }
-        //十字キー
-        if (Mathf.Abs(LeftCrossHorizontal) > 0.05f || Mathf.Abs(LeftCrossVertical) > 0.05f)
-        {
-            print("LeftCrossHorizontal:" + LeftCrossHorizontal);
-            print("LeftCrossVertical:" + LeftCrossVertical);
-        }
-        //右スティック
-        if (Mathf.Abs(RightStickHorizontal) > 0.05f || Mathf.Abs(RightStickVertical) > 0.05f)
-        {
-            print("RightStickHorizontal:" + RightStickHorizontal);
-            print("RightStickVertical:" + RightStickVertical);
-        }
-        //LRTrigger
-        if (Mathf.Abs(LRTrigger) > 0.05f)
-        {
-            print("LRTrigger:" + LRTrigger);
-        }
-        ////////////////////////////
-        //GetButton
-        //ViewButton
-        if (Input.GetButtonDown("ViewButton"))
-        {
-            print("ViewButton");
-        }
-        //MenuButton
-        if (Input.GetButtonDown("MenuButton"))
-        {
-            print("MenuButton");
-        }
-        //ADownButton
-        if (Input.GetButtonDown("ADownButton"))
-        {
-            print("ADownButton");
-        }
-        //BRightButton
-        if (Input.GetButtonDown("BRightButton"))
-        {
-            print("BRightButton");
-        }
-        //XLeftButton
-        if (Input.GetButtonDown("XLeftButton"))
-        {
-            print("XLeftButton");
-        }
-        //YUpButton
-        if (Input.GetButtonDown("YUpButton"))
-        {
-            print("YUpButton");
-        }
         //LButton
-        if (Input.GetButtonDown("LButton"))
+        if (Input.GetButtonDown("LButton")&& LeftRotateFlag == false && RightRotateFlag == false)
         {
             //transform.Rotate(0, 0, 90);
             print("LButton");
+            oldTargetAngels = currentTargetAngels;
             LeftRotateFlag = true;
-
+            if (oldTargetAngels == Quaternion.Euler(0, 0, 0f))
+            {
+                currentTargetAngels = Quaternion.Euler(0, 0, 90f);
+            }
+            if (oldTargetAngels == Quaternion.Euler(0, 0, 90f))
+            {
+                currentTargetAngels = Quaternion.Euler(0, 0, 180f);
+            }
+            if (oldTargetAngels == Quaternion.Euler(0, 0, -180f) || oldTargetAngels == Quaternion.Euler(0, 0, 180f))
+            {
+                currentTargetAngels = Quaternion.Euler(0, 0, -90f);
+            }
+            if (oldTargetAngels == Quaternion.Euler(0, 0, -90f))
+            {
+                currentTargetAngels = Quaternion.Euler(0, 0, 0f);
+            }
         }
         //RButton
-        if (Input.GetButtonDown("RButton"))
+        if (Input.GetButtonDown("RButton") && LeftRotateFlag == false && RightRotateFlag == false)
         {
+            oldTargetAngels = currentTargetAngels;
             //transform.Rotate(0, 0, -90);
             print("RButton");
             RightRotateFlag = true;
+            if (oldTargetAngels == Quaternion.Euler(0, 0, 0f))
+            {
+                currentTargetAngels = Quaternion.Euler(0, 0, -90f);
+            }
+            if (oldTargetAngels == Quaternion.Euler(0, 0, 90f))
+            {
+                currentTargetAngels = Quaternion.Euler(0, 0, 0f);
+            }
+            if (oldTargetAngels == Quaternion.Euler(0, 0, -180f) || oldTargetAngels == Quaternion.Euler(0, 0, 180f))
+            {
+                currentTargetAngels = Quaternion.Euler(0, 0, 90f);
+            }
+            if (oldTargetAngels == Quaternion.Euler(0, 0, -90f))
+            {
+                currentTargetAngels = Quaternion.Euler(0, 0, 180f);
+            }
         }
-        //LeftStickDown
-        if (Input.GetButtonDown("LeftStickDown"))
-        {
-            print("LeftStickDown");
-        }
-        //RightStickDown
-        if (Input.GetButtonDown("RightStickDown"))
-        {
-            print("RightStickDown");
-        }
+
 
         //LeftRotation
         if (LeftRotateFlag)
         {
-            OldAngle = CurrentAngle;
-            if ((transform.eulerAngles.z - OldAngle) < 90)
+            //StageRotation
+            Player.useGravity = false;  //Default状況でPlayerの重力がない
+            MoveFlag = false;
+            //  用 slerp 进行插值平滑的旋转
+            transform.rotation = Quaternion.Slerp(transform.rotation, currentTargetAngels, rotateSpeed * Time.deltaTime);
+            // 当初始角度跟目标角度小于1,将目标角度赋值给初始角度,让旋转角度是我们需要的角度
+            if (Quaternion.Angle(currentTargetAngels, transform.rotation) < 1)
             {
-                transform.Rotate(0, 0, Time.deltaTime * 90 * 2, Space.World);
+                transform.rotation = currentTargetAngels;        //旋转到目标角度     
+                LeftRotateFlag = false;                          //停止旋转停止旋转,恢复到可以旋转的状态
             }
-            else
+            //PlayerRotation
+            //  用 slerp 进行插值平滑的旋转
+            PlayerTransform.rotation = Quaternion.Slerp(PlayerTransform.rotation, currentTargetAngels, rotateSpeed * Time.deltaTime);
+            // 当初始角度跟目标角度小于1,将目标角度赋值给初始角度,让旋转角度是我们需要的角度
+            if (Quaternion.Angle(currentTargetAngels, transform.rotation) < 1)
             {
-                LeftRotateFlag = false; //停止
-                CurrentAngle = transform.eulerAngles.z;
+                transform.rotation = currentTargetAngels;        //旋转到目标角度     
+                LeftRotateFlag = false;                          //停止旋转停止旋转,恢复到可以旋转的状态
             }
-        }
+        }     
         //RightRotation
-        if (RightRotateFlag)
+        else if (RightRotateFlag)
         {
-            OldAngle = CurrentAngle;
-            if ((transform.eulerAngles.z - OldAngle) < -90)
+            Player.useGravity = false;  //Default状況でPlayerの重力がない
+            MoveFlag = false;
+            //  用 slerp 进行插值平滑的旋转
+            transform.rotation = Quaternion.Slerp(transform.rotation, currentTargetAngels, rotateSpeed * Time.deltaTime);
+            // 当初始角度跟目标角度小于1,将目标角度赋值给初始角度,让旋转角度是我们需要的角度
+            if (Quaternion.Angle(currentTargetAngels, transform.rotation) < 1)
             {
-                transform.Rotate(0, 0, -Time.deltaTime * 9 * 2, Space.World);
-            }
-            else
-            {
-                RightRotateFlag = false; //停止
-                CurrentAngle = transform.eulerAngles.z;
+                transform.rotation = currentTargetAngels;        //旋转到目标角度     
+                RightRotateFlag = false;                         //停止旋转,恢复到可以旋转的状态
             }
 
         }
+        else
+        {
+            Player.useGravity = true;  //Default状況でPlayerの重力があります
+            if (FootCollision.grounded == true)
+            {
+                MoveFlag = true;
+            }
+            
+        }
+        
     }
 
 }
